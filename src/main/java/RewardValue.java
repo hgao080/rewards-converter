3fb9c74bfcd260a16543a4a6c8fbdfb269b88a3d
Henry Gao <hgao080@aucklanduni.ac.nz>
2024-03-04 12:06:45 +1300

    Added two contructors and two methods to meet requirements

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,22 @@
+import java.lang.Math;
+
 public class RewardValue {
+    double cash;
+
+    public RewardValue(double cashValue) {
+        cash = cashValue;
+    }
+
+    public RewardValue(int milesValue) {
+        cash = Math.round(milesValue * 0.0035 * 100.0) / 100.0;
+    }
+
+    public double getCashValue() {
+        return cash;
+    }
+
+    public double getMilesValue() {
+        return (int)(cash / 0.0035);
+    }
+
 }
